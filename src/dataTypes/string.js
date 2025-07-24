80eb518c2fbd1fcdedc662068a852bf8163eadcc
Hiep asus <hiepad333@gmail.com>
2025-07-24 14:37:44 +0700

    String methods: includes, replace, repeat, slice

--- src/dataTypes/string.js
+++ src/dataTypes/string.js
@@ -33,4 +33,32 @@ console.log(sentence.startsWith('The')); // Output: true
 console.log(sentence.endsWith('dog.')); // Output: true
 console.log(sentence.endsWith('lazy.')); // Output: true
 
+//includes: kiểm tra chuỗi con 
+console.log(sentence.includes('quick')); // Output: true
+console.log(sentence.includes('cat')); // Output: false
+
+//replace: thay thế chuỗi con 
+const originalText = 'I love JavaScript JavaScript!';
+const newText = originalText.replace('JavaScript', 'programming');// Output: I love programming!
+const newText2 = originalText.replaceAll('JavaScript', 'programming'); // Thay thế tất cả 
+const newText3 = originalText.replace('asdasd', 'programming'); // Không tìm thấy chuỗi con, không thay thế gì
+console.log(newText); // Output: I love programming!
+console.log(newText2); // Output: I love programming programming!
+console.log(newText3); // Output: I love JavaScript JavaScript!
+
+// repeat: lặp lại chuỗi
+const repeatedString = 'Hello! '.repeat(3);
+console.log(repeatedString); // Output: Hello! Hello! Hello!
+
+//slice: cắt chuỗi
+// slice(start, end) - cắt từ vị trí start đến end (không bao gồm end, nếu không có end thì cắt đến hết chuỗi, nếu start âm thì tính từ cuối chuỗi, nếu)
+const slicedString = 'Hello, world!'.slice(0, 5);
+console.log(slicedString); // Output: Hello
+const slicedString2 = 'Hello, world!'.slice(7);
+console.log(slicedString2); // Output: world!
+const slicedString3 = 'Hello, world!'.slice(-6);
+console.log(slicedString3); // Output: world!
+const slicedString4 = 'Hello, world!'.slice(-6, -1);
+console.log(slicedString4); // Output: world
+
 
